1abe2eded28478f7c49bc1395c74d2cb58a9d611
NickVerbeet <n.verbeet@student.fontys.nl>
2017-11-25 16:38:15 +0200

    Adds name field to POST

--- src/routes/user.js
+++ src/routes/user.js
@@ -17,7 +17,7 @@ router.get('/', (req, res) => {
 
 router.post('/', (req, res) => {
   const newUser = new User();
-  newUser.name = 'Nick';
+  newUser.name = req.body.name;
   newUser.save((err) => {
     if (err) {
       console.error(err);
