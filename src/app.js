4bf6b2a1f8f314e05fcdaeaacf2858f4d5f9b6da
NickVerbeet <n.verbeet@student.fontys.nl>
2017-11-25 16:45:56 +0200

    Sets bodyparser urlencoded to true

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 app.use(session({ secret: 'keyboard cat', cookie: { secure: true } }));
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(`${__dirname}/public`));
 
